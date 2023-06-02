2ff72cf843c3fab530f6192e5c99ab9231bdbd32
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2023-06-03 03:24:41 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <header className='w-full flex justify-center items-center flex-col'>
             <nav className='flex justify-between items-center w-full mb-10 pt-3'>
                 <img src={logo} alt="summary_logo" className='w-28 object-contain' />
-                <button type='button' className='black_btn' onClick={() => window.open('https://github.com/bhuyand143')}>GitHub</button>
+                <button type='button' className='black_btn' onClick={() => window.open('https://github.com/bhuyand143/ai_summarizer')}>GitHub</button>
             </nav>
             <h1 className='head_text'>
                 Summarize Articles with <br className='max-md:hidden' />
